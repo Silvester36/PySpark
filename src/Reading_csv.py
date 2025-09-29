48853646f216e2b4c5d66e59d14660566db1e289
Silvester36 <silvester.g@diggibyte.com>
2025-09-29 17:33:32 +0530

    Ignored files

--- src/Reading_csv.py
+++ src/Reading_csv.py
@@ -2,6 +2,8 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName('p3').getOrCreate()
 
-df = spark.read.csv(path="D:\Sales.csv",header=True)
+df = spark.read.csv(path="D:\Sales.csv",header=True,inferSchema=True)
 
 df.show()
+
+df.printSchema()
