e94dacecd18f2637ed093bd561913da7eb3618f2
Silvester36 <silvester.g@diggibyte.com>
2025-10-01 15:00:42 +0530

    Using Split in a DataFrame to split the elements

--- src/Data Frame/Split.py
+++ src/Data Frame/Split.py
@@ -5,11 +5,11 @@ from pyspark.sql.functions import col, explode, split
 df = [("Silvester",['Python,Java']),("Roshan",['C++,C'])]
 
 column = StructType([StructField("Name",StringType()),
-                     StructField("Numbers",StringType())])
+                     StructField("Skills",StringType())])
 
 spark = SparkSession.builder.appName("Df6").getOrCreate()
 df = spark.createDataFrame(df,column)
 
-df2 = df.withColumn("Numbers",split(col('Numbers'),','))
+df2 = df.withColumn("Skills",split(col('Skills'),','))
 
 df2.show()
