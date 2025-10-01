dc1c3ae1176876677c4b0aea23de422116b90afd
Silvester36 <silvester.g@diggibyte.com>
2025-10-01 15:49:32 +0530

    To check whether the array contains the value

---
+++ src/Data Frame/Array_Contains.py
@@ -0,0 +1,17 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType,StructField,StringType
+from pyspark.sql.functions import col, array,array_contains
+
+df = [("Silvester",'Python','Java'),("Roshan",'C++','C')]
+
+column = StructType([StructField("Name",StringType()),
+                     StructField("Skill1",StringType()),
+                     StructField("Skill2",StringType())])
+
+spark = SparkSession.builder.appName("Df6").getOrCreate()
+df = spark.createDataFrame(df,column)
+
+df2 = df.withColumn("Skills",array(col('Skill1'),col('Skill2')))
+
+df3 = df2.withColumn('Skill',array_contains("Skills",'Java'))
+df3.show()
